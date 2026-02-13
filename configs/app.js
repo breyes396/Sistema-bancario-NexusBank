330b66995b80ef0345203a5f8f8ab759e2d445cb
breyes396 <breyes-2024396@kinal.edu.gt>
2026-02-12 23:42:02 -0600

    C02: Se creó conexión a MongoDB

---
+++ configs/app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import { dbConnection } from './db.js';
+import { corsOptions } from './cors-configuration.js';
+import { helmetConfiguration } from './helmet-configuration.js';
+
+const BASE_PATH = '/nexusBank/v1';
+
+const middlewares = (app) => {
+    app.use(express.urlencoded({extended: false, limit: '10mb'}));
+    app.use(express.json({limit: '10mb'}));
+    app.use(cors(corsOptions));
+    app.use(helmet(helmetConfiguration));
+    app.use(morgan('dev'));
+}
+
+const routes = (app) => {
+    app.use((req, res) =>{
+        res.status(404).json({
+            success: false,
+            message: 'Endpoint no encontrado en Api'
+        })
+    })
+}
+
+export const initServer = async () => {
+    const app = express();
+    const PORT = process.env.PORT;
+    app.set('trus proxy', 1);
+
+    try {
+        await dbConnection();
+        middlewares(app);
+        routes(app);
+
+        app.listen(PORT, ()=> {
+            console.log(`NexusBank server running on port ${PORT}`);
+            console.log(`Health check: http://localhost:${PORT}${BASE_PATH}/health`);
+        })
+    } catch (error) {
+        console.error(`Error starting Admin Server: ${error.message}`);
+        process.exit(1);
+    }
+}
